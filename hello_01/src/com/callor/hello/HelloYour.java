714072c779477d24fc3b09b842e48fc74d552f2e
marpolll <minstarsoso@gmail.com>
2023-04-13 10:17:37 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/HelloYour.java
@@ -0,0 +1,17 @@
+package com.callor.hello;
+/*
+ *  Source Code, Java Class File
+ *  
+ */
+
+public class HelloYour {
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+		
+	}
+
+}
