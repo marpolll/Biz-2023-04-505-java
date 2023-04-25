d62ec03118bcc2282f77b47133683478b61576b4
marpolll <minstarsoso@gmail.com>
2023-04-25 18:38:57 +0900

    0425home

--- java_008_Class/src/com/marpol/classes/arrays/ArrayF.java
+++ java_008_Class/src/com/marpol/classes/arrays/ArrayF.java
@@ -29,7 +29,7 @@ public class ArrayF {
 	public static void main(String[] args) {
 
 		int[] nums = new int[10];
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10; i++) {
 			nums[i] = (int) (Math.random() * 50) + 51;
 			}
 			for(int i = 0 ; i < 10 ; i++) {
